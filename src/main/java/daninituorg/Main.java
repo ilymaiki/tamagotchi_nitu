60649566d3d215efe0a90ae1683d0f7c65cbdcb2
Nitu <daninitu2006@gmail.com>
2026-01-02 18:03:28 +0100

    Mostrar estado del Tamagotchi

--- src/main/java/daninituorg/Main.java
+++ src/main/java/daninituorg/Main.java
@@ -2,6 +2,12 @@ package daninituorg; // Programa desarrollado por Daniel Nitu Velinov.
 import java.util.Scanner;
 
 public class Main {
+
+        // Variables del Tamagotchi.
+        static int saciedad = 6;
+        static int energia = 6;
+        static int diversion = 6;
+
     public static void main(String[] args) {
         int opcion = menuPrincipal();
         System.out.println("Opcion elegida: " +opcion);
@@ -34,5 +40,39 @@ public class Main {
 
         } while (opcion < 0 || opcion > 4);
         return opcion;
+    }
+
+    private static void mostrarEstado() {
+
+        if (saciedad == 0 || energia == 0 || diversion == 0) { // Estado de GAME OVER.
+            System.out.println("(x_x) :( Game Over ):");
+            System.out.println(" /|_|\\");
+            System.out.println("  | |");
+            return;
+        }
+
+        if (saciedad > 5 && energia > 55 && diversion >5) { // Estado de felicidad máxima.
+            System.out.println("(•‿•) ¡Estoy feliz!");
+            System.out.println(" /|_|\\");
+            System.out.println("  | |");
+        }
+
+        if (diversion <=4) { // Estado de aburrimiento.
+            System.out.println("(-︵-) Estoy aburrido");
+            System.out.println(" /|_|\\ ¡Juega conmigo!");
+            System.out.println(" / \\");
+        }
+
+        else if (energia <= 4) { // Estado de baja energia.
+            System.out.println("(-_-) Zzz");
+            System.out.println(" /|_|\\");
+            System.out.println("  | |");
+        }
+
+        else if (saciedad <= 4) { // Estado de hambre.
+            System.out.println("(•︵•) ¡Tengo hambre! 🍗");
+            System.out.println(" /|x|\\");
+            System.out.println("  | |");
+        }
     }
 }
